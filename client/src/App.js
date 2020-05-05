e400eae045dec01f56fdb17bb94f991cd08ab6df
aivis <aiviskle@gmail.com>
2020-05-05 14:15:39 +0300

    improve chart

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { testData } from "./testData";
-// import { TimePicker } from "antd";
-// import { Button } from "antd";
-// import { ArrowRightOutlined } from "@ant-design/icons";
-import { Button } from "@material-ui/core";
-// import DateFnsUtils from "@date-io/date-fns";
-import {
-  KeyboardTimePicker,
-  MuiPickersUtilsProvider,
-} from "@material-ui/pickers";
+import { Button, Grid, Box } from "@material-ui/core";
+import { TimePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
 import MomentUtils from "@date-io/moment";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 import {
   Bar,
@@ -20,131 +12,128 @@ import {
   Legend,
   ComposedChart,
   Line,
+  Tooltip,
 } from "recharts";
 import "./App.css";
-import moment from "moment";
 
 function App() {
-  const format = "HH:mm";
-  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const theme = createMuiTheme({
+    palette: {
+      type: "dark",
+    },
+  });
 
-  const theme = React.useMemo(
-    () =>
-      createMuiTheme({
-        palette: {
-          type: prefersDarkMode ? "dark" : "light",
-        },
-      }),
-    [prefersDarkMode]
-  );
-
-  const [selectedDate, setSelectedDate] = React.useState(
-    new Date("2014-08-18T21:11:54")
-  );
-
-  const handleDateChange = (date) => {
-    setSelectedDate(date);
-  };
+  const [fromTime, setFromTime] = useState(new Date("2020-01-01T23:00:00"));
+  const [toTime, setToTime] = useState(new Date("2020-01-02T07:00:00"));
 
   const data = [
     {
-      date: "Monday",
-      rec: 8,
-      sleep: 7.5,
-      nap: 1,
+      date: "Mon",
+      Recommended: 8,
+      Sleep: 7.5,
+      Nap: 1,
     },
     {
-      date: "Tuesday",
-      rec: 8,
-      sleep: 9,
-      nap: 0,
+      date: "Tue",
+      Recommended: 8,
+      Sleep: 9,
+      Nap: 0,
     },
     {
-      date: "Wednesday",
-      rec: 8,
-      sleep: 6,
-      nap: 0,
+      date: "Wed",
+      Recommended: 8,
+      Sleep: 6,
+      Nap: 0,
     },
     {
-      date: "Thursday",
-      rec: 8,
-      sleep: 8.5,
-      nap: 0,
+      date: "Thu",
+      Recommended: 8,
+      Sleep: 5,
+      Nap: 0,
+    },
+    {
+      date: "Fri",
+      Recommended: 8,
+      Sleep: 10,
+      Nap: 0,
+    },
+    {
+      date: "Sat",
+      Recommended: 8,
+      Sleep: 7,
+      Nap: 0,
+    },
+    {
+      date: "Sun",
+      Recommended: 8,
+      Sleep: 8.5,
+      Nap: 2,
     },
   ];
+
+  const timePickerStyle = {
+    marginRight: 20,
+    width: 100,
+  };
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
-        <div
-          style={{
-            margin: "50px auto 0",
-            width: "fit-content",
-          }}
-        >
+      <Box mt={5}>
+        <Grid container direction="column" justify="center" alignItems="center">
           <ComposedChart
-            width={500}
+            style={{ marginLeft: -50 }}
+            width={400}
             height={300}
             data={data}
-            // margin={{
-            //   top: 5,
-            //   right: 20,
-            //   left: 20,
-            //   bottom: 5,
-            // }}
-            barSize={40}
           >
             <CartesianGrid strokeDasharray="3 3" />
             <XAxis dataKey="date" />
             <YAxis />
-            {/* <Tooltip /> */}
+            <Tooltip />
             <Legend />
-            <Bar dataKey="sleep" stackId="a" fill="#1E90FF" />
-            <Bar dataKey="nap" stackId="a" fill="#00BFFF" />
+            <Bar dataKey="Sleep" stackId="a" fill="#3f51b5" />
+            <Bar dataKey="Nap" stackId="a" fill="#5DA9E9" />
             <Line
-              dataKey="rec"
-              stroke="#7B68EE"
+              dataKey="Recommended"
+              stroke="#D1345B"
               dot={false}
               activeDot={false}
             />
           </ComposedChart>
-          <div
-            style={{
-              margin: "30px 0 0 30px",
-              textAlign: "center",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            {/* <TimePicker
-            style={{ marginRight: 10 }}
-            defaultValue={moment("22:00", format)}
-            format={format}
-          />
-          <ArrowRightOutlined />
-          <TimePicker
-            style={{ marginLeft: 10 }}
-            defaultValue={moment("07:00", format)}
-            format={format}
-          />
-        <Button type="primary">Add</Button> */}
-            <MuiPickersUtilsProvider utils={MomentUtils}>
-              <KeyboardTimePicker
-                margin="normal"
-                id="time-picker"
-                label="Time picker"
-                value={selectedDate}
-                onChange={handleDateChange}
-                KeyboardButtonProps={{
-                  "aria-label": "change time",
-                }}
-              />
-            </MuiPickersUtilsProvider>
-            <Button color="primary">Add</Button>
-          </div>
-        </div>
-      </div>
-      <div>Hello</div>
+
+          <MuiPickersUtilsProvider utils={MomentUtils}>
+            <Box mt={5}>
+              <Grid
+                container
+                direction="row"
+                justify="center"
+                alignItems="center"
+              >
+                <TimePicker
+                  clearable
+                  autoOk
+                  ampm={false}
+                  label="From"
+                  value={fromTime}
+                  onChange={setFromTime}
+                  style={timePickerStyle}
+                />
+                <TimePicker
+                  clearable
+                  autoOk
+                  ampm={false}
+                  label="To"
+                  value={toTime}
+                  onChange={setToTime}
+                  style={timePickerStyle}
+                />
+                <Button variant="contained" color="primary">
+                  Add
+                </Button>
+              </Grid>
+            </Box>
+          </MuiPickersUtilsProvider>
+        </Grid>
+      </Box>
     </ThemeProvider>
   );
 }
