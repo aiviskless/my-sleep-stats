d438a7699688f8ad80fdf03f865f39c54873078f
aivis <aiviskle@gmail.com>
2020-05-10 21:41:57 +0300

    add helmet, compress

--- server.js
+++ server.js
@@ -2,10 +2,23 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 const config = require("config");
-
+const compression = require("compression");
+const helmet = require("helmet");
 const app = express();
 
+app.use(compression()); // Compress all routes
+// serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("./client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.use(express.json());
+app.use(helmet());
 
 const db = config.get("mongoURI");
 mongoose
@@ -20,16 +33,6 @@ mongoose
 app.use("/api/items", require("./routes/api/items"));
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
-
-// serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-  // set static folder
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 
 const port = process.env.PORT || 5000;
 
